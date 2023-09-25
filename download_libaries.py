d52e8a139e88e6a2e3f22018732aa55e9ad92d9b
NightDev701 <lukasbaehr01@gmail.com>
2023-09-25 11:03:30 +0200

    basic stuff

--- download_libaries.py
+++ download_libaries.py
@@ -1,5 +1,7 @@
+import os
+
 mysql_link = "https://sunlightdev.org/api/sunlightscorpion/libary/3323cccf-068f-4ef4-97c7-9aec1cb3a521/mysql-connector-java-8.0.29.jar"
 targetFolder = "API"
 
-
-
+if not os.path.exists(targetFolder):
+    os.makedirs(targetFolder)
