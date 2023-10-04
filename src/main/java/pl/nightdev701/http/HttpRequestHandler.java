8ad84d85335739fc5edef7d780cf4a0bf7154695
GreenBloodDev <baehr.lukas01@gmail.com>
2023-10-04 11:16:36 +0200

    add download function [#2]

--- src/main/java/pl/nightdev701/http/HttpRequestHandler.java
+++ src/main/java/pl/nightdev701/http/HttpRequestHandler.java
@@ -1,18 +1,34 @@
 package pl.nightdev701.http;
 
+import pl.nightdev701.logger.AbstractLogger;
+import pl.nightdev701.logger.standard.DefaultLogger;
+
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.logging.Level;
 
 public class HttpRequestHandler {
 
     private final String url;
+    private final AbstractLogger logger;
     private String body;
 
     public HttpRequestHandler(String url) {
+        this.logger = new DefaultLogger();
         this.url = url;
+    }
+
+    public HttpRequestHandler(String url, AbstractLogger logger) {
+        this.url = url;
+        this.logger = logger;
     }
 
     /**
@@ -30,6 +46,39 @@ public class HttpRequestHandler {
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    /**
+     * Download file
+     */
+    public void download(String output) {
+
+        String link = url;
+
+        logger.log(Level.INFO, "Download file to path: " + output);
+
+        URL url;
+        try {
+            url = new URL(link);
+            try (
+                    InputStream inputStream = url.openStream();
+                    BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
+                    FileOutputStream fileOutputStream = new FileOutputStream(output)
+            ) {
+                byte[] bucket = new byte[2048];
+                int numBytesRead;
+
+                while ((numBytesRead = bufferedInputStream.read(bucket, 0, bucket.length)) != -1) {
+                    fileOutputStream.write(bucket, 0, numBytesRead);
+                }
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Download failed: " + e.getMessage());
+            }
+            logger.log(Level.INFO, "Download complete!");
+        } catch (MalformedURLException e) {
+            logger.log(Level.WARNING, "Download failed: " + e.getMessage());
+            throw new RuntimeException(e);
         }
     }
 
