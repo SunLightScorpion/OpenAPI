8b29746256670da5173bf02d0ca2e6779faaf49b
Lukas <Lukasbaehr01@gmail.com>
2023-10-05 18:57:51 +0200

    testing.

--- download_libaries.py
+++ download_libaries.py
@@ -1,7 +1,12 @@
 import os
+import urllib.request
 
-mysql_link = "https://sunlightdev.org/api/sunlightscorpion/libary/3323cccf-068f-4ef4-97c7-9aec1cb3a521/mysql-connector-java-8.0.29.jar"
+mysql_link = "http://45.85.218.127/api/libary/3323cccf-068f-4ef4-97c7-9aec1cb3a521/mysql-connector-j-8.1.0.jar"
+target_mysql_file = "API/mysql-connector-j-8.1.0.jar"
+
 targetFolder = "API"
 
 if not os.path.exists(targetFolder):
     os.makedirs(targetFolder)
+
+urllib.request.urlretrieve(mysql_link, target_mysql_file)
