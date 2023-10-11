095a4cde0a1d2fcb257e9abc476407111867ca3c
XNonymous <56768074+XNonymous-N2@users.noreply.github.com>
2023-10-11 18:02:28 +0200

    Updated  DatabaseType, better implementation for default port

--- src/main/java/pl/nightdev701/database/type/DatabaseType.java
+++ src/main/java/pl/nightdev701/database/type/DatabaseType.java
@@ -3,24 +3,21 @@ package pl.nightdev701.database.type;
 /* database types */
 public enum DatabaseType {
 
-    MYSQL,
-    POSTGRESQL,
+    MYSQL(3306),
+    POSTGRESQL(5432),
     ;
 
+    private final int defaultPort;
+
+    DatabaseType(int defaultPort) {
+        this.defaultPort = defaultPort;
+    }
+
     /**
-     * Gets the default port for the database type.
-     *
-     * @return the default port for the database type (3306 for MySQL, 5432 for PostgresSQL).
+     * Gets the default port for a database type
+     * @return defaultPort
      */
     public int getDefaultPort() {
-        int port = -1;
-
-        if (this == MYSQL) {
-            port = 3306;
-        } else if (this == POSTGRESQL) {
-            port = 5432;
-        }
-
-        return port;
+        return defaultPort;
     }
 }
