716077fb8e4a92aa8a890bce7a5652df8b09c517
NightDev701 <lukasbaehr01@gmail.com>
2025-01-21 23:40:36 +0100

    base for database server

---
+++ src/main/java/pl/nightdev701/database/luma/LumaCoreDatabase.java
@@ -0,0 +1,41 @@
+package pl.nightdev701.database.luma;
+
+
+/*
+
+lukas - 11:15 PM, 1/21/25
+https://github.com/NightDev701
+
+© SunLightScorpion 2020 - 2024
+
+*/
+
+import com.google.gson.Gson;
+import pl.nightdev701.OpenAPI;
+import pl.nightdev701.network.HttpRequestHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LumaCoreDatabase {
+
+    private final String address;
+
+    public LumaCoreDatabase(String address) {
+        this.address = address;
+    }
+
+    public void insertData(String database, HashMap<String, String> rec) {
+        HttpRequestHandler http = OpenAPI.getRequestHandler(address + "/" + database);
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", "Bearer your_token");
+        headers.put("Content-Type", "application/json");
+
+        Gson gson = new Gson();
+        String dataToJson = gson.toJson(rec);
+
+        http.makeApiRequest("POST", headers, database);
+    }
+
+}
